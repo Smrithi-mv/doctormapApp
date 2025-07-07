4b833bf7caa2405a36deebdf2ea4f308bf8690e3
Smrithi MV <smrithimv2003@gmail.com>
2025-07-07 16:16:06 +0530

    oopss

--- src/main/java/com/shanInfotech/DoctorListMapApp/App.java
+++ src/main/java/com/shanInfotech/DoctorListMapApp/App.java
@@ -9,14 +9,14 @@ import com.shanInfotech.DoctorListMapApp.exception.SpecialisationCheckException;
  */
 public class App {
     public static void main(String[] args) throws SpecialisationCheckException, MissingFieldException {
-       // System.out.println("Hello World!");
-    	DoctorDao dd=new DoctorDao();
-    	dd.addDoctor();
-    	dd.getDoctors();
-    	//dd.sortByDoctorName();
-    	//dd.sortByHospital();
-    	//dd.sortBySpecialisation();
-    	
+        System.out.println("Hello World!");
+//    	DoctorDao dd=new DoctorDao();
+//    	dd.addDoctor();
+//    	dd.getDoctors();
+//    	//dd.sortByDoctorName();
+//    	//dd.sortByHospital();
+//    	//dd.sortBySpecialisation();
+//    	
     	
     }
 }
